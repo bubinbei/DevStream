d59f2a7dcb2d40c57cf67af796abee6f08b6299d
bubinbei <bubinbei@yandex.ru>
2024-10-07 05:53:24 +0400

    addPrisma

--- expressApi/app.js
+++ expressApi/app.js
@@ -1,26 +1,27 @@
-var createError = require('http-errors'); // Модуль для создания HTTP-ошибок.
-var express = require('express'); // Основной модуль Express.js.
-var path = require('path'); // Модуль для работы с путями.
-var cookieParser = require('cookie-parser'); // Middleware для парсинга куки.
-var logger = require('morgan'); // Middleware для логирования HTTP-запросов (в данном случае используется morgan).
+const createError = require('http-errors'); // Модуль для создания HTTP-ошибок.
+const express = require('express'); // Основной модуль Express.js.
+const path = require('path'); // Модуль для работы с путями.
+const cookieParser = require('cookie-parser'); // Middleware для парсинга куки.
+const logger = require('morgan'); // Middleware для логирования HTTP-запросов (в данном случае используется morgan).
 
-var indexRouter = require('./routes/index'); // Маршруты для основной страницы.
-var usersRouter = require('./routes/users'); // Маршруты для пользователей.
+const indexRouter = require('./routes'); 
+const upload = express.static('uploads')
 
-var app = express(); // Экземпляр приложения Express.
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views')); // Устанавливает путь к директории с шаблонами.
-app.set('view engine', 'jade'); // Устанавливает шаблонизатор (в данном случае jade, который теперь называется pug).
+const app = express(); 
 
 app.use(logger('dev')); // Middleware для логирования запросов.
 app.use(express.json()); // Middleware для парсинга JSON-запросов.
 app.use(express.urlencoded({ extended: false })); // Middleware для парсинга URL-кодированных запросов.
 app.use(cookieParser()); // Middleware для парсинга куки.
-app.use(express.static(path.join(__dirname, 'public'))); // Middleware для обслуживания статических файлов из директории public.
+app.set('view engine', 'jade'); // Устанавливает шаблонизатор (в данном случае jade, который теперь называется pug).
 
-app.use('/', indexRouter); // Использует маршруты для основной страницы.
-app.use('/users', usersRouter); // Использует маршруты для пользователей.
+app.use('/uploads', upload)
+
+app.use('/api', indexRouter);
+
+if(!false.existsSync('uploads')) {
+  fs.mkdirSync('uploads');
+} 
 
 // Middleware для перехвата запросов, которые не соответствуют 
 // ни одному маршруту, и передачи их в обработчик ошибок.
@@ -28,15 +29,11 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message; // Устанавливает сообщение об ошибке.
 
+app.use(function(err, req, res, next) {
+  res.locals.message = err.message; // Устанавливает сообщение об ошибке.
   // Устанавливает объект ошибки, если приложение работает в режиме разработки.
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500); //Устанавливает статус ответа.
   res.render('error'); // Рендерит страницу ошибки.
 });
--- expressApi/routes/index.js
+++ expressApi/routes/index.js
@@ -1,9 +1,21 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const multer = require('multer');
+
+const uploadDestination = 'uploads'
+// место хранения файлов
+const storage = multer.diskStorage({
+    destination: 'uploads',
+    filename: function (req, file, cb) {
+        cb(null, file.originalname)
+    }
+});
+
+const upload = multer({ storage: storage });
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/register', function(req, res, next) {
+    res.send('register');
 });
 
 module.exports = router;
